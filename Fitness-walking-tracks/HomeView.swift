9831310d6c0f1ac7a1307984900fa71439cfd329
Mark Ching <manho445@gmail.com>
2023-02-24 15:35:12 +0800

    HomeView UI Update

--- Fitness-walking-tracks/HomeView.swift
+++ Fitness-walking-tracks/HomeView.swift
@@ -10,7 +10,7 @@ struct HomeView: View {
     @AppStorage("log_status") var logStatus: Bool = false
 
     
-//    let item = Bundle.main.decode([FacilityElement].self, from:"facillity-fw.json")
+
     var body: some View {
         //init()
         VStack(){
@@ -40,37 +40,17 @@ struct HomeView: View {
                 
             }
             HStack{
-//                TextField("Search...",text: $text)
-//                    .padding(7)
-//                    .padding(.horizontal,25)
-//                    .shadow(color: .black, radius: 10)
-//                    .background(Color(.systemGray3))
-//
-//                    .cornerRadius(8)
-//                    .padding(.horizontal,10)
-//                    .onTapGesture {
-//                        self.Search = true
-//                    }
-//                if Search{
-//                    Button(action:{
-//                        self.Search = false
-//                        self.text = ""
-//                    })
-//                    {
-//                        Text("Cancel")
-//                    }
-//                    .padding(.trailing, 10)
-//                    .transition(.move(edge: .trailing))
-//                }
+
             }
             
-                HStack{
+            HStack{
                     Text("推薦路線")
                         .padding(10)
                         .fontWeight(.bold)
                         .foregroundColor(.blue)
                         .frame(maxWidth: .infinity,alignment: .leading)
                 }
+            
             HStack{
                 Button(action:{},label: {
                     Image(systemName: "gear")
@@ -78,8 +58,8 @@ struct HomeView: View {
                         .aspectRatio( contentMode: .fit)
                         .frame(width: 200,height: 200)
                 })
-            
             }
+            
             VStack{
                 Text("全部路線")
                     .padding(10)
@@ -89,53 +69,22 @@ struct HomeView: View {
                 
                 List {
                     ForEach(FW, id: \.titleEn) { facility in
-                        NavigationLink("\(facility.titleEn)", destination: DetailView(Fw: facility)
+                        NavigationLink{
+                            DetailView(Fw: facility)
+                        }label: {
                             
-                        )
+                            AsyncImage(url: URL(string: "\(facility.mapURLEn)")){
+                                Image in Image
+                                    .resizable()
+                            }placeholder: {
+                            }.frame(width: 90,height: 90)
+                        Text("\(facility.titleEn)")
+                        }
                     }
-                }
+                }.frame(width: 440)
+            
             }
-//            HStack{
-//                    List {
-//                        ForEach(FW, id: \.titleEn) { facility in
-//                            NavigationLink("\(facility.titleEn)", destination: DetailView(Fw: facility))
-//                        }
-//                    }
-//                }
-            
-
-            
-//            NavigationStack{
-//                List{
-//                    ForEach(menu) {section in Text(Section.name)
-//
-//                        ForEach(section.item) {item in ItemRow(item:item)}
-//                    }
-//                }
-//                .navigationTitle("Hello")
-//            }
-        
-                
-            
         }
-
-       
-        VStack(spacing: 20){
-            if logStatus{
-                Text("Logged In")
-                
-                Button("Loggout") {
-                    try? Auth.auth().signOut()
-                    logStatus = false
-                }
-                
-            }else{
-                Text("Came as Guset")
-            }
-            
-        }
-        .navigationBarBackButtonHidden(false)
-        .navigationTitle("Home")
         
     }
 }
