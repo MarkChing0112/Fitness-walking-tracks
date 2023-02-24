c3caf4816dd17f3039461e432b978b89d9ea7a01
Holung123 <118161483+Holung123@users.noreply.github.com>
2023-02-24 20:58:57 +0800

    updateSearchBar

--- Fitness-walking-tracks/HomeView.swift
+++ Fitness-walking-tracks/HomeView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import Firebase
 
 struct HomeView: View {
-    
+    @State var searchQuery = ""
     private var FW: [FacilityElement] = FacilityElement.fWalking
     
     // Log Status
@@ -14,35 +14,7 @@ struct HomeView: View {
     var body: some View {
         //init()
         VStack(){
-          
-            HStack(){
-                
-                Text("Fitness Walking Tracks")
-                    .padding(10)
-                    .fontWeight(.bold)
-                    .font(.title)
-                //                    Image(systemName: "gear").resizable().aspectRatio( contentMode: .fit).frame(width: 50,height: 50)
-                Spacer()
-                if logStatus{
-                    Button(action:{
-                        try? Auth.auth().signOut()
-                        logStatus = false},label: {
-                        Image(systemName: "rectangle.portrait.and.arrow.forward")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 40,height: 40)
-                            .padding(10)
-                    })
-                    
-                }else{
-                    Text("Came as Guset")
-                }
-                
-            }
-            HStack{
 
-            }
-            
             HStack{
                     Text("推薦路線")
                         .padding(10)
@@ -53,10 +25,12 @@ struct HomeView: View {
             
             HStack{
                 Button(action:{},label: {
-                    Image(systemName: "gear")
-                        .resizable()
-                        .aspectRatio( contentMode: .fit)
-                        .frame(width: 200,height: 200)
+                    AsyncImage(url: URL(string: "\(FW[1].mapURLEn)")){
+                        Image in Image
+                            .resizable()
+                    }placeholder: {
+                    }.frame(width: 400,height: 200)
+
                 })
             }
             
@@ -82,12 +56,40 @@ struct HomeView: View {
                         }
                     }
                 }.scaleEffect(x: 1.2 ,y: 1.029, anchor:.bottom)
+                    .searchable(text: $searchQuery)
+                    //                .navigationTitle("Fitness Walking Tracks")
+                    .navigationBarItems( leading:
+                                            HStack{Image("runningIcon")
+                            .resizable()
+                            .frame(width: 40,height: 40)
+                        Text("Fitness Walking Tracks")
+                        //                    .padding(10)
+                            .fontWeight(.bold)
+                        //                        .font(.title)
+                            .font(.system(size: 20))
+                    }
+                                         ,trailing: HStack{if logStatus{
+                                             Button(action:{
+                                                 try? Auth.auth().signOut()
+                                                 logStatus = false},label: {
+                                                     Image(systemName: "rectangle.portrait.and.arrow.forward")
+                                                         .resizable()
+                                                         .aspectRatio( contentMode: .fit)
+                                                         .frame(width: 30,height: 30)
+                                                        
+                                                 })
+                                             
+                                         }else{
+                                             Text("Came as Guset")
+                                         }
+                    }
+                    )}
                     
             }
         }
         
     }
-}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
